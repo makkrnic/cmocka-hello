6ada74d4801ef6e0f78d72ce7852dacf73fd1b37
Mak Krnic <mak@makkrnic.com>
2016-07-19 08:41:18 +0200

    Add failing test
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

--- tests/almost-calc-sum.c
+++ tests/almost-calc-sum.c
@@ -26,11 +26,16 @@ static void torture_sum_3_5(void **state) {
 	assert_int_equal(sum(3, 5), 8);
 }
 
+static void failing_sum(void **state) {
+	assert_int_equal(sum(1, 1), 3);
+}
+
 int main(void) {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(torture_sum_1_1),
 		cmocka_unit_test(torture_sum_1_2),
 		cmocka_unit_test(torture_sum_3_5),
+		cmocka_unit_test(failing_sum),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
